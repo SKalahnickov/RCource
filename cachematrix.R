d0bd68c2190695c5402c7baaa1914995074f1b0a
Sergey <svkalashnickov@gmail.com>
2018-11-05 22:27:10 +0300

    Added makeCacheMatrix func

---
+++ cachematrix.R
@@ -0,0 +1,19 @@
+## Put comments here that give an overall description of what your
+## functions do
+
+## A wrapping over the matrix
+
+makeCacheMatrix <- function(x = matrix()) {
+  i <- NULL
+  set <- function(y) {
+    x <<- y
+    i <<- NULL
+  }
+  get <- function() x
+  setInverse <- function(inverse) i <<- inverse
+  getInverse <- function() i
+  list(set = set, get = get,
+       setInverse = setInverse,
+       getInverse = getInverse)
+}
+
